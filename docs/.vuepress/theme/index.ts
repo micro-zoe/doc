4cf436ccfe682a3d7c910089a0e5937d44f904cb
liyuan97 <liyuan97@jd.com>
2024-01-31 18:32:30 +0800

    feat: navbar

--- docs/.vuepress/theme/index.ts
+++ docs/.vuepress/theme/index.ts
@@ -1,19 +1,23 @@
-import type { ThemeData } from '@vuepress/plugin-theme-data';
+import type { ThemeData } from '@vuepress/plugin-theme-data'
 import type { Theme } from '@vuepress/core'
 import { getDirname, path } from '@vuepress/utils'
-import { defaultTheme, type DefaultThemePluginsOptions, DefaultThemeLocaleData } from '@vuepress/theme-default'
+import {
+  defaultTheme,
+  type DefaultThemePluginsOptions,
+  DefaultThemeLocaleData
+} from '@vuepress/theme-default'
 
 const __dirname = getDirname(import.meta.url)
 
-type DefaultThemeLocaleOptions = DefaultThemeData;
-type DefaultThemeData = ThemeData<MicroAppThemeLocaleData>;
+type DefaultThemeLocaleOptions = DefaultThemeData
+type DefaultThemeData = ThemeData<MicroAppThemeLocaleData>
 
 interface MicroAppThemeLocaleData extends DefaultThemeLocaleData {
   anchorToc?: string
 }
 
 interface MicroAppThemeOptions extends DefaultThemeLocaleOptions {
-  themePlugins?: DefaultThemePluginsOptions;
+  themePlugins?: DefaultThemePluginsOptions
 }
 
 const MicroAppTheme = (options: MicroAppThemeOptions): Theme => {
@@ -22,8 +26,11 @@ const MicroAppTheme = (options: MicroAppThemeOptions): Theme => {
     extends: defaultTheme(options),
 
     alias: {
-      // '@theme/NavbarItems.vue': path.resolve(__dirname, './components/NavbarItems.vue'),
-    },
+      '@theme/NavbarItems.vue': path.resolve(
+        __dirname,
+        './components/NavbarItems.vue'
+      )
+    }
   }
 }
 
