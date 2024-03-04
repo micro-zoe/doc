02de8a450002df76b1428ad89b0eb994bf0f0578
CrystalAngelLee <34746593+CrystalAngelLee@users.noreply.github.com>
2024-03-04 02:11:02 +0000

    Deploying to gh-pages from @ micro-zoe/doc@aaedac20f9f01e6e148a5008afaf2526c2ee2db2 🚀

--- assets/TabPanel-PLpUqJbs.js
+++
@@ -1,1 +0,0 @@
-import{f as t,o as s,c as l,i as n,n as r,_ as o}from"./app-eF4imkDp.js";const c=t({__name:"TabPanel",props:{title:{type:String,required:!0},active:{type:Boolean,required:!1,default:!1}},setup(e){return(a,i)=>(s(),l("div",{class:r(["micro-tab-panel",{active:e.active}])},[n(a.$slots,"default")],2))}}),p=o(c,[["__file","TabPanel.vue"]]);export{p as default};
